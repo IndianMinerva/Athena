738791e99eec26c1d8e2dff9407c3098372b1a97
Arun Merum <arunkumar.m@above-inc.com>
2014-06-08 01:54:03 +0530

    Corrected a few errors and added 'new_book' view

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'slim'
 require 'sass'
 
+enable :sessions
+
 require_relative 'business'
 require_relative 'mongo_config'
 
@@ -18,8 +20,19 @@ post '/login' do
 	password = params[:password]
 
 	if User.where(:username => username,  :password => password).any?
+		session["user"] = username
 		"Good!"
 	else
+		@error = "Invalid credentials"
+		slim :login
+	end
+end
+
+get '/book' do
+	if session["user"]
+		slim :new_book
+	else
+		@error = "Please login"
 		slim :login
 	end
 end
