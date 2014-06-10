f270cdf3286dbdde856f97a1b3298778a4401dc3
Arun Merum <arunkumar.m@above-inc.com>
2014-06-10 12:40:17 +0530

    add, list and view views for the books

--- business.rb
+++ business.rb
@@ -9,3 +9,20 @@ class User
     key :last_name,  String
     timestamps!
 end
+
+class Book
+    include MongoMapper::Document
+     
+    key :title,       String,   :required => true
+    key :author,      String,   :required => true
+    key :price,       Float
+    key :type,        String
+    key :publisher,   String
+    key :language,    String
+    key :pages,       Integer
+    key :cover,       Binary
+    key :cover_name,  String
+    key :description, String
+    key :genere,      String
+    timestamps!
+end
--- main.rb
+++ main.rb
@@ -24,11 +24,11 @@ post '/login' do
 		"Good!"
 	else
 		@error = "Invalid credentials"
-		slim :login
+		slim :login 
 	end
 end
 
-get '/book' do
+get '/book/new' do
 	@title="New Book"
 	if session["user"]
 		slim :new_book
@@ -36,4 +36,31 @@ get '/book' do
 		@error = "Please login"
 		slim :login
 	end
+end
+
+get '/book/:id' do
+	@book = Book.find(params['id'])
+	@book.to_s
+	if @book.nil?
+		"No such book"
+	else
+		slim :view_book
+	end
+end
+
+post '/book' do
+	cover = params["cover"]
+	params.delete("cover")
+	book = Book.new(params)
+	if cover
+		book[:cover] = cover[:tempfile].read
+		book[:cover_name] = cover[:name]
+	end
+	book.save
+	book.keys.to_s
+end
+
+get '/book/list' do
+	@books = Book.all
+	slim :list_book
 end
